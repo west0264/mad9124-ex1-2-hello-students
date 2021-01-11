2174c4f3888032d7f2e9923ae9e41e1d0ea4eb10
Ivan Westra <ivanwestra@gmail.com>
2021-01-11 18:05:11 -0500

    finished the exercise

--- helloStudents.js
+++ helloStudents.js
@@ -2,8 +2,31 @@
 
 // 1. Read the JSON file into a variable called students
 
+const students = require('./students.json');
+
 // 2. Iterate over the students array and print Hello with their full names to the console
+
+function say(message) {
+    console.log(message)
+};
+
+
+let counter = 0;
+
+students.forEach(student => {
+    const firstName = student.firstName; 
+    const lastName = student.lastName; 
+    say(`Hello ${firstName} ${lastName}`);
+    if (lastName.charAt(0) == "D") {
+        counter += 1;
+    } 
+}); 
+
+
 // e.g. Hello Walter Baker
 
 // 3. Print out the number of last names starting with the letter D
+say("Count of last names starting with D is" + " " + counter);
+
 // e.g. Count of last names starting with D is 1
+
